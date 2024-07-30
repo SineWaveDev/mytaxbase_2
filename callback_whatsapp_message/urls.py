a781d09d67145e2ecdfbfbb6d41ae82d7e925966
SineWaveDev <admin@sinewave.co.in>
2024-07-30 15:22:20 +0530

    Initial commit88

--- callback_whatsapp_message/urls.py
+++ callback_whatsapp_message/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SendCampaignAPIView
+from .views import SendCampaignAPIView, SendCampaignAPI2View
 
 urlpatterns = [
     path('send-campaign/', SendCampaignAPIView.as_view(), name='send-campaign'),
+    path('send-campaign2/', SendCampaignAPI2View.as_view(), name='send-campaign2'),
 ]
--- callback_whatsapp_message/views.py
+++ callback_whatsapp_message/views.py
@@ -30,3 +30,51 @@ class SendCampaignAPIView(APIView):
             return Response({"message": "Campaign sent successfully"}, status=status.HTTP_200_OK)
         else:
             return Response({"error": "Failed to send campaign"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+import requests
+import logging
+from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework import status
+
+# Configure logging
+logger = logging.getLogger(__name__)
+
+class SendCampaignAPI2View(APIView):
+    parser_classes = [JSONParser]
+
+    def post(self, request):
+        mobile_number = request.data.get('mobile_number')
+        tokan_number = request.data.get('tokan_number')
+
+        if not mobile_number:
+            return Response({"error": "Mobile number is required"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if not tokan_number:
+            return Response({"error": "Tokan number is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        payload = {
+            "apiKey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGRmMmM4MGMzZjY5MGU4MTY3Nzc3OSIsIm5hbWUiOiJTaW5ld2F2ZSBDb21wdXRlciBzZXJ2aWNlcyIsImFwcE5hbWUiOiJBaVNlbnN5IiwiY2xpZW50SWQiOiI2NGRkZjJjODBjM2Y2OTBlODE2Nzc3NzQiLCJhY3RpdmVQbGFuIjoiTk9ORSIsImlhdCI6MTY5MjI2NzIwOH0.r5cHWBFTMs_4F70F4z3bb6MGTmkfSN1oOOM5PSaOUT0",
+            "campaignName": "helpline",
+            "destination": mobile_number,
+            "userName": "Sagar",
+            "templateParams": [
+                tokan_number 
+            ]
+        }
+
+        try:
+            response = requests.post("https://backend.api-wa.co/campaign/smartping/api", json=payload)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Failed to send campaign: {e}")
+            return Response({"error": "Failed to send campaign"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if response.status_code == 200:
+            return Response({"message": "Campaign sent successfully"}, status=status.HTTP_200_OK)
+        else:
+            logger.error(f"Unexpected response status: {response.status_code}")
+            return Response({"error": "Failed to send campaign"}, status=status.HTTP_400_BAD_REQUEST)
