0fd4f832907efa06fe2b703172a7846e6b77512c
SineWaveDev <admin@sinewave.co.in>
2025-01-24 11:51:39 +0530

    first commit55

--- angel_one/urls.py
+++ angel_one/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import StartangleLogin, ProvideOTPAngle, DownloadFile
+from .views import StartangleLogin, ProvideOTPAngle
 
 urlpatterns = [
     path('DownloadangeloneReport/', StartangleLogin.as_view(), name='DownloadangeloneReport'),
     path('ProvideOTPAngle/', ProvideOTPAngle.as_view(), name='ProvideOTPAngle'),
-    path("download-files/", DownloadFile.as_view(), name="download_files"),  # New endpoint for downloading files
 ]
